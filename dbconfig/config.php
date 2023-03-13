3c1fdfa9b8d7f53576fd6a93c7b9b12e74eefdef
junglin0916 <105787653+junglin0916@users.noreply.github.com>
2023-03-13 08:56:54 +0800

    Delete config.php

--- dbconfig/config.php
+++
@@ -1,8 +0,0 @@
-
-//before implementing this create a folder named dbconfig and paste this file in it. 
-
-
-<?php
-$con = mysqli_connect("localhost","root","") or die("Unable to connect");
-mysqli_select_db($con,'fuck');
-?>
